75612e93abc9256dfdfcb97e8dacfc24acd7bab8
wabegopr98 <rabchuk.serezha2017@yandex.ru>
2025-01-11 17:08:37 +0300

    Homework with unit tests

--- test/test_app.py
+++ test/test_app.py
@@ -8,10 +8,20 @@ def test_add():
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
 
+def test_subtract():
+    """Тестирует функцию вычитания"""
+    assert subtract(10, 5) == 5
+    assert subtract(6,3) == 3
+
+def test_multiply():
+    """Тестирует функцию умножения"""
+    assert multiply(5,5) == 25
+    assert multiply(10,10) == 100
+
 
 def test_divide():
     """Тестирует функцию деления."""
     assert divide(10, 2) == 5
     assert divide(9, 3) == 3
-    # with pytest.raises(ValueError):
-    #     divide(5, 0)
+    with pytest.raises(ValueError):
+        divide(5, 0)
